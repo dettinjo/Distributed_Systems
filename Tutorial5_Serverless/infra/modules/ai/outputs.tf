c503a63b7e04d15bf91bf4b0e689bd7e0a921c66
Joel Dettinger <j.dettinger@student.vu.nl>
2026-01-25 18:47:28 +0100

    updated code

--- Tutorial5_Serverless/infra/modules/ai/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "endpoint" {
-  value = azurerm_cognitive_account.vision.endpoint
-}
-
-output "key" {
-  value     = azurerm_cognitive_account.vision.primary_access_key
-  sensitive = true
-}
--- Tutorial5_Serverless/infra/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-resource_group_name = "serverless-lab-rg"
-location            = "swedencentral"
-environment         = "student-lab"
